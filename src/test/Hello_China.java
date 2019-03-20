2a88bfac8e2987c8ebf4b29bc7d84e0cdbe47fe0
tan_glb <tan_glb@qq.com>
2019-03-20 15:39:26 +0800

    china

--- src/test/Hello_China.java
+++ src/test/Hello_China.java
@@ -3,6 +3,6 @@ package test;
 public class Hello_China {
     public static void main(String[] args)
     {
-        System.out.println("你好，中国！");
+        System.out.println("你好,china！");
     }
 }
